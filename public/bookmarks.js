0aa0f35a26d821c77a5517bffaa9c97430977fa4
fl97 <50992060+fl97@users.noreply.github.com>
2020-10-22 10:22:36 +0200

    deployed with firebase

---
+++ public/bookmarks.js
@@ -0,0 +1,24 @@
+const bookmarks = [
+    {
+        title: "Daily",
+        links: [
+            { name: "Mail", url: "https://gmx.de" },
+            { name: "GitHub", url: "https://github.com/fl97" },
+        ],
+    },
+    {
+        title: "Entertainment",
+        links: [
+            { name: "Youtube", url: "https://youtube.com" },
+            { name: "Netflix", url: "https://netflix.com" },
+            { name: "Amazon Prime", url: "https://www.amazon.de/Amazon-Video/b?ie=UTF8&node=3010075031"},
+        ],
+    },
+    {
+        title: "Social",
+        links: [
+            { name: "Instagram", url: "https://instagram.com" },
+            { name: "Facebook", url: "https://facebook.com" },
+        ],
+    },
+];
